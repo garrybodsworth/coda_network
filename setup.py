4e5418daf69b237f3f53ca0aca3c1cf7f79e2016
Garry Bodsworth <gjb@camvine.com>
2011-06-28 09:45:15 +0100

    Up version number and add script to the setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 # encoding: utf-8
 from setuptools import setup, find_packages
 
-VERSION_STRING = '1.0'
+VERSION_STRING = '1.5'
 
 setup(
     name = 'coda_network',
     version = VERSION_STRING,
     packages = find_packages(),
+    scripts = ['mini_proxy.py', ]
 
     author = 'Cambridge Visual Networks',
     author_email = 'support@camvine.com',
